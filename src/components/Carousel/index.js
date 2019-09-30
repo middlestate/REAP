2bdbc4da5dd7aed0744ed1271c2a9c605daa864f
pahosler <pahosler@gmail.com>
2019-09-30 11:39:42 -0500

    added carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,69 +1,73 @@
-import React, { Component } from 'react'
-import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import Slider from 'react-slick'
 
 // const carousels = bulmaCarousel.attach('.carousel', {
 //   slidesToScroll: 1,
 //   slidesToShow: 1,
 //   loop: true,
 // })
+const PrevArrow = ({ className, style, onClick }) => (
+  <div
+    className={className}
+    style={{ ...style, height: 20, width: 20, paddingTop: 2, display: 'block', background: 'black', borderRadius: 25 }}
+    onClick={onClick}
+  />
+)
 
-class Carousel extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      slideLeft: true,
-    }
-    this.handleSlide = this.handleSlide.bind(this)
-  }
+const NextArrow = ({ className, style, onClick }) => (
+  <div
+    className={className}
+    style={{ ...style, height: 20, width: 20, paddingTop: 2, display: 'block', background: 'black', borderRadius: 25 }}
+    onClick={onClick}
+  />
+)
 
-  handleSlide (e) {
-    e.target.id === 'prev' ? this.carousels.prev() : this.carousels.next()
-  }
+const settings = {
+  autoplay: true,
+  autoplaySpeed: 6000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  prevArrow: <PrevArrow />,
+  nextArrow: <NextArrow />,
+}
 
-  componentDidMount () {
-    const carousels = bulmaCarousel.attach('.carousel', {
-      slidesToScroll: 1,
-      slidesToShow: 1,
-      loop: true,
-    })
-  }
-
-  render () {
-    const { heading, cards } = this.props
-    return (
-      <div className='columns is-centered is-multiline'>
-        <div className='column is-half has-text-centered'>
-          <h1 className='is-size-1 has-text-REAP-gray'>{heading}</h1>
-        </div>
-        <div
-          className='column is-three-quarters is-offset-5'
-          style={{ overflow: 'hidden' }}>
-          <div className='carousel' id='carousel'>
-            {cards.map(({ title, text }, keys) => (
-              <div key={keys} className={`item-${keys + 1}`}>
-                <div className='card' style={{ width: '50%' }}>
-                  <header className='card-header'>
-                    <p
-                      className='card-header-title has-text-REAP-orange is-size-3'
-                      style={{ flex: 1, justifyContent: 'center' }}>
-                      {title}
-                    </p>
-                  </header>
-                  <div className='card-content' style={{ margin: 20 }}>
-                    <div className='content'>
-                      <span style={{ padding: 20, margin: 20 }}>{text}</span>
-                    </div>
+const Carousel = ({ heading, cards }) => (
+  <Fragment>
+    <div className='columns is-centered '>
+      <div className='column is-half has-text-centered'>
+        <h1 className='is-size-1 has-text-REAP-gray'>{heading}</h1>
+      </div>
+    </div>
+    <div className='columns is-centered'>
+      <div className='column is-4'>
+        <Slider {...settings}>
+          {cards.map(({ title, text }, keys) => (
+            <div key={keys} className={`item-${keys + 1}`}>
+              <div className='card is-borderless is-shadowless'>
+                <header className='card-header'>
+                  <p
+                    className='card-header-title has-text-REAP-orange is-size-3'
+                    style={{ flex: 1, justifyContent: 'center' }}>
+                    {title}
+                  </p>
+                </header>
+                <div className='card-content' style={{ margin: 20 }}>
+                  <div className='content'>
+                    <span style={{ padding: 20, margin: 20 }}>{text}</span>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-        </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    )
-  }
-}
+    </div>
+  </Fragment>
+)
 
 Carousel.propTypes = {
   heading: PropTypes.string,
